2cbeb3c9b99b808d536a6923b5f5cf0b8f89b37f
Elayne Gomes <elaynegomes@gmail.com>
2020-10-22 01:26:01 -0300

    Commit de acompanhamento das aulas de 1 a 19.

--- src/main/java/com/elaynegomes/api/ProjetoSpringBootApplication.java
+++ src/main/java/com/elaynegomes/api/ProjetoSpringBootApplication.java
@@ -1,14 +1,38 @@
 package com.elaynegomes.api;
 
+import com.elaynegomes.api.utils.SenhaUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjetoSpringBootApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qrdPorPagina;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
 		System.out.println("Iniciando Projeto Spring Boot");
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+//		return args -> {
+//			System.out.println("### Quantidade de elementos por página: " + this.qrdPorPagina);
+//		};
+
+		return args -> {
+			String senhaEncoded = SenhaUtils.geraBCrypt("123456");
+			System.out.println("Senha Encoded: " + senhaEncoded);
+
+			senhaEncoded = SenhaUtils.geraBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);
+
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
 	}
 
 }
--- src/test/java/com/elaynegomes/api/ProjetoSpringBootApplicationTests.java
+++ src/test/java/com/elaynegomes/api/ProjetoSpringBootApplicationTests.java
@@ -1,13 +1,29 @@
 package com.elaynegomes.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjetoSpringBootApplicationTests {
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCarregarContextoDeTeste() {
+		assertEquals(100, qtdPorPagina);
 	}
 
 }
